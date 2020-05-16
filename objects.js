83b1916151bf322c3b361f6dd8a7d38cc85d88f7
Sahej Singh <sahejdeep@hotmail.com>
2020-05-16 02:18:21 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,3 +2,5 @@ var playlist = { Drake: "Energy" };
 
 function updatePlaylist(playlist, artistName, songTitle) {playlist[artistName] = songTitle
 return playlist}
+
+function removeFromPlaylist(playlist, artistName) {delete playlist.artistName}
